232110d4d3bf1a93ff5dd677fab1905a1a917f9d
Ankur Shrivastava <ankur@ankurs.com>
2020-06-06 15:20:24 +0800

    updating support for multiple monitor

--- rc.lua
+++ rc.lua
@@ -64,7 +64,7 @@ run_once({
     "blueman-applet",
     "variety -n", -- change wallpaper
     "xautolock -time 5 -locker 'xlock -mode space'",
-    -- "xrandr --output eDP-1 --scale 1x1 --mode 1680x1050" -- for laptop
+    -- "xrandr --output eDP-1 --scale 1x1 --mode 1680x1050" -- for laptop when things break
 }) -- entries must be separated by commas
 
 -- This function implements the XDG autostart specification
@@ -808,4 +808,59 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 -- possible workaround for tag preservation when switching back to default screen:
 -- https://github.com/lcpz/awesome-copycats/issues/251
+local tag_store = {}
+tag.connect_signal("request::screen", function(t)
+  local fallback_tag = nil
+
+  -- find tag with same name on any other screen
+  for other_screen in screen do
+    if other_screen ~= t.screen then
+      fallback_tag = awful.tag.find_by_name(other_screen, t.name)
+      if fallback_tag ~= nil then
+        break
+      end
+    end
+  end
+
+  -- no tag with same name exists, chose random one
+  if fallback_tag == nil then
+    fallback_tag = awful.tag.find_fallback()
+  end
+
+  if not (fallback_tag == nil) then
+    local output = next(t.screen.outputs)
+
+    if tag_store[output] == nil then
+      tag_store[output] = {}
+    end
+
+    clients = t:clients()
+    tag_store[output][t.name] = clients
+
+    for _, c in ipairs(clients) do
+      c:move_to_tag(fallback_tag)
+    end
+  naughty.notify({ text = "Moved tags" })
+
+  end
+end)
+
+screen.connect_signal("added", function(s)
+  local output = next(s.outputs)
+  naughty.notify({ text = output .. " Connected" })
+
+  tags = tag_store[output]
+  if not (tags == nil) then
+    naughty.notify({ text = "Restoring Tags" })
+
+    for _, tag in ipairs(s.tags) do
+      clients = tags[tag.name]
+      if not (clients == nil) then
+        for _, client in ipairs(clients) do
+          client:move_to_tag(tag)
+        end
+      end
+    end
+  end
+end)
 -- }}}
